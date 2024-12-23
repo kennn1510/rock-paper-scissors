6b1a1db0a767abbc5fa4e2d583a1e99b8e02f907
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2024-12-24 00:46:15 +0700

    Add playRound logic

--- javascript.js
+++ javascript.js
@@ -1,1 +1,54 @@
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  console.log(randomNumber);
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else if (randomNumber === 2) {
+    return "scissors";
+  }
+}
+// console.log(getComputerChoice());
+
+/*
+Create function called getHumanChoice
+Create variable to store user input
+Prompt user's input(string), recommend words like "rock", "paper", or "scissors"
+Return user's input
+Log function to test it
+*/
+function getHumanChoice() {
+  let userInput = prompt("Type your choice of either rock, paper, or scissors");
+  return userInput;
+}
+// console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  if (humanChoice === computerChoice) {
+    return "A tie! " + humanChoice + " ties with " + computerChoice;
+  } else if (
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    return console.log("You win! " + humanChoice + " beats " + computerChoice);
+  } else if (
+    (humanChoice === "scissors" && computerChoice === "rock") ||
+    (humanChoice === "paper" && computerChoice === "scissors") ||
+    (humanChoice === "rock" && computerChoice === "paper")
+  ) {
+    computerScore++;
+    return console.log("You lose! " + computerChoice + " beats " + humanChoice);
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
