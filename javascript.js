549929581c2b2d5ec8968ad472c8dc49603280bf
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2024-12-26 17:55:20 +0700

    Added barebones UI with buttons

--- javascript.js
+++ javascript.js
@@ -1,9 +1,15 @@
+let gameOver = false;
 let humanScore = 0;
 let computerScore = 0;
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const restartBtn = document.querySelector("#restart");
+const runningScore = document.querySelector("#running-score");
+const div = document.querySelector("#container");
 
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
-  //   console.log(randomNumber);
   if (randomNumber === 0) {
     return "rock";
   } else if (randomNumber === 1) {
@@ -12,54 +18,31 @@ function getComputerChoice() {
     return "scissors";
   }
 }
-// console.log(getComputerChoice());
-
-/*
-Create function called getHumanChoice
-Create variable to store user input
-Prompt user's input(string), recommend words like "rock", "paper", or "scissors"
-Return user's input
-Log function to test it
-*/
-function getHumanChoice() {
-  let userInput = prompt("Type your choice of either rock, paper, or scissors");
-  return userInput;
-}
-// console.log(getHumanChoice());
-
 function playRound(humanChoice, computerChoice) {
   humanChoice = humanChoice.toLowerCase();
-  if (humanChoice === computerChoice) {
-    return console.log(
-      "A tie! " +
-        capitalizeFirstLetter(humanChoice) +
-        " ties with " +
-        capitalizeFirstLetter(computerChoice)
-    );
-  } else if (
+  if (
     (humanChoice === "paper" && computerChoice === "rock") ||
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "scissors" && computerChoice === "paper")
   ) {
     ++humanScore;
-    return console.log(
-      "You win! " +
-        capitalizeFirstLetter(humanChoice) +
-        " beats " +
-        capitalizeFirstLetter(computerChoice)
-    );
   } else if (
     (humanChoice === "scissors" && computerChoice === "rock") ||
     (humanChoice === "paper" && computerChoice === "scissors") ||
     (humanChoice === "rock" && computerChoice === "paper")
   ) {
     ++computerScore;
-    return console.log(
-      "You lose! " +
-        capitalizeFirstLetter(computerChoice) +
-        " beats " +
-        capitalizeFirstLetter(humanChoice)
-    );
+  }
+  runningScore.textContent = `Your score: ${humanScore} vs. Computer score: ${computerScore}`;
+  div.style.background = "yellow";
+  if (humanScore >= 5) {
+    runningScore.textContent = `You win! Your score: ${humanScore}, Opponent score: ${computerScore}`;
+    div.style.background = "green";
+    gameOver = true;
+  } else if (computerScore >= 5) {
+    runningScore.textContent = `You lose! Your score: ${humanScore}, Opponent score: ${computerScore}`;
+    div.style.background = "red";
+    gameOver = true;
   }
 }
 
@@ -68,21 +51,21 @@ function capitalizeFirstLetter(str) {
 }
 
 function playGame() {
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
-  }
-  if (humanScore === computerScore) {
-    return console.log(
-      `A tie! Your score: ${humanScore}, Opponent score: ${computerScore}`
-    );
-  } else if (humanScore > computerScore) {
-    return console.log(
-      `You win! Your score: ${humanScore}, Opponent score: ${computerScore}`
-    );
-  } else {
-    return console.log(
-      `You lose! Your score: ${humanScore}, Opponent score: ${computerScore}`
-    );
-  }
+  rockBtn.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+  });
+  paperBtn.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+  });
+  scissorsBtn.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice());
+  });
+  restartBtn.addEventListener("click", () => {
+    gameOver = false;
+    humanScore = 0;
+    computerScore = 0;
+    runningScore.textContent = "Running Score:";
+    div.style.background = "white";
+  });
 }
-playGame();
+if (gameOver === false) playGame();
